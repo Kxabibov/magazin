3533ef4d67d44f5b011d3bb0357aac3a39fa9290
Kxabibov <habibovkomron007@gmail.com>
2026-05-31 18:51:08 +0500

    Fix Express v5 path matching syntax for wildcard route from * to /*

--- server.ts
+++ server.ts
@@ -265,7 +265,7 @@ async function startServer() {
     console.log('Serving production static files from dist...');
     const distPath = path.join(__dirname, 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
